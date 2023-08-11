b3a209eae2ffac6b6406ab7d96a76d48154520bf
vikasaries <58937814+vikasaries@users.noreply.github.com>
2023-08-11 19:11:52 +0530

    Create generateOAuthToken.js

---
+++ .github/workflows/generateOAuthToken.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+
+async function generateOAuthToken() {
+  try {
+    const tokenUrl = 'https://oauth.example.com/token'; // Replace with the actual token endpoint
+    const clientId = process.env.CLIENT_ID; // Set this as a repository secret
+    const clientSecret = process.env.CLIENT_SECRET; // Set this as a repository secret
+    const grantType = 'client_credentials'; // or other grant type as needed
+
+    const response = await axios.post(tokenUrl, null, {
+      params: {
+        client_id: clientId,
+        client_secret: clientSecret,
+        grant_type: grantType,
+      },
+    });
+
+    if (response.status === 200) {
+      return response.data.access_token;
+    } else {
+      throw new Error('OAuth token generation failed');
+    }
+  } catch (error) {
+    console.error('Error generating OAuth token:', error.message);
+    throw error;
+  }
+}
+
+generateOAuthToken()
+  .then(token => {
+    console.log('Generated OAuth Token:', token);
+    console.log(`::set-output name=oauth_token::${token}`);
+  })
+  .catch(error => {
+    console.error('OAuth token generation failed:', error.message);
+    process.exit(1);
+  });
