24bda38aa889a9283c4c5d734a36fbcb097f4b8c
vikasaries <58937814+vikasaries@users.noreply.github.com>
2023-09-06 19:58:34 +0530

    Update getoauthToken.js

--- getoauthToken.js
+++ getoauthToken.js
@@ -7,14 +7,14 @@ async function generateOAuthToken() {
     const clientSecret = process.env.CLIENT_SECRET; // Set this as a repository secret
     const grantType = 'client_credentials'; // or other grant type as needed
 
-    const response = await axios.post(tokenUrl, null, {
-      // auth: {
-      //   username: clientId,
-      //   password: clientSecret
-      // },
-      headers:{
-        Authorization: 'Basic c2ItZTMyNzJjNWQtZGVkNy00Yjk3LTk4ZjUtYjJlOTM4M2Q1ZTk4IWI3MjU3fGl0IWIyNTk6ODEwYjc3MDQtZjExNi00MDQzLWJjODctODlhOTYxNDk4NGI2JFJPLUtmOUQ3a2hqclAwMTR4Z1RYQndwYUx5TDlkU2Jmb1V6NUVUZEdtUEE9'
+    const response = await axios.post(tokenUrl, {
+      auth: {
+        username: clientId,
+        password: clientSecret
       },
+      // headers:{
+      //   Authorization: 'Basic c2ItZTMyNzJjNWQtZGVkNy00Yjk3LTk4ZjUtYjJlOTM4M2Q1ZTk4IWI3MjU3fGl0IWIyNTk6ODEwYjc3MDQtZjExNi00MDQzLWJjODctODlhOTYxNDk4NGI2JFJPLUtmOUQ3a2hqclAwMTR4Z1RYQndwYUx5TDlkU2Jmb1V6NUVUZEdtUEE9'
+      // },
       params: {
         // client_id: clientId,
         // client_secret: clientSecret,
